1f12a1d1ef72d26de50398239679bd1282128ceb
Sofya02 <nikolaenko.sofya@mail.ru>
2023-06-27 17:27:40 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'light_blue': '#E5F2FF',
+        'dark_blue': '#060545',
+        'light_grey': '#565A69',
+        'text_grey':'#B7B7B7',
+        'n_white':'#FCFCFC',
+        'n_grey':'#DBDBDB',
+        'n_blue':'#63AFFF',
+        'n2_blue':'#9DCCFF',
+        'text_d_blue':'#17335F',
+        'b2_blue':'#CCE4FE',
+        'b2_2_blue':'#99CAFE',
+        'b_w':'#F4FAFF',
+        'dark_li_blue':'#007BFC',
+        'green_1':'#51AD77',
+        'beige':'#FFD999',
+        'pink':'#F3C8CF',
+        'black_1':'#00001A',
+        'blue_f':'#007BFB',
+        'grey_2':'#BDC5DB',
+        't_g_2':'#4A5368',
+        'red':'red',
+        'gr':'#6D768E',
+        'dark_b':'#082A60',
+        'light_blue_1_box':'#C5DCFF',
+        'light_pink':'#FFF1F4',
+        'li_gr_t':'#7F879C',
+
+      },
+    },
+  },
+  plugins: [],
+}
+
